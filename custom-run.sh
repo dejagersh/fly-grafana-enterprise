4d7d682683be5c5d3f3245c29b798e9da750e675
Johan de Jager <johan@laptify.nl>
2023-08-16 13:46:25 +0200

    Wip

--- custom-run.sh
+++ custom-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 crudini --set /etc/grafana/grafana.ini smtp password hello
+crudini --set /etc/grafana/grafana.ini server.custom_response_headers example hello
 
 usermod --shell /bin/bash grafana
 
